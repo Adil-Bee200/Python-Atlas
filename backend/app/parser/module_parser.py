65dfee893b7a631e04408210c683e3111a6fbfa6
Adil <Adilbhatti8008@gmail.com>
2026-07-01 22:57:37 -0700

    Implemented class for import parser

--- backend/app/parser/module_parser.py
+++ backend/app/parser/module_parser.py
@@ -1,13 +1,42 @@
 from pathlib import Path
 from backend.app.models.import_models import ParsedModule, ParsedImport, ParsedResult
 from backend.app.models.scan_models import ScanResult
+import ast 
+
+class ImportVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.imports : list[ParsedImport] = []
+    
+    def visit_Import(self, node: ast.Import):
+        for alias in node.names:
+            self.imports.append(
+                ParsedImport(
+                    raw_import=f"import {alias.name}",
+                    module_name=alias.name,
+                )
+            )
+    
+    def visit_ImportFrom(self, node: ast.ImportFrom):
+        for alias in node.names:
+            module_name = node.module or ""
+            imported_names = ", ".join(alias.name for alias in node.names)
+            self.imports.append(
+                ParsedImport(
+                    raw_import=f"from {module_name} import {imported_names}",
+                    module_name=module_name,
+                )
+            )
+
+
 
 def parse_module(module_path: Path) -> ParsedModule:
     # TODO: Reads one module and returns a ParsedModule object
-    
-    return ParsedModule(path= module_path, module_path= module_path.module_path, imports= tuple())
+
+    return parse_module(module_path)
 
 def parse_all_modules(scan_result: ScanResult) -> ParsedResult:
     # TODO: Reads all modules in the scan result and returns a ParsedResult object
+
+
 
     return ParsedResult(repo_root= scan_result.repo_root, modules= tuple())
