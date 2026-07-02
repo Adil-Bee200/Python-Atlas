a5d64d079ffb175413b5b2674b7dfadcd935cabe
Adil <Adilbhatti8008@gmail.com>
2026-07-02 15:49:02 -0700

    Added additional tests for parser

--- backend/tests/test_module_parser.py
+++ backend/tests/test_module_parser.py
@@ -3,6 +3,7 @@ from backend.app.parser.module_parser import parse_module
 from backend.app.models.scan_models import PythonModule
 
 def test_parse_module_extracts_top_level_imports(tmp_path):
+    ## test: a module that imports only at the top-level
     app_dir = tmp_path / "app"
     app_dir.mkdir()
 
@@ -48,3 +49,108 @@ def main():
     }
 
     assert len(parsed.imports) == 4
+
+def test_parse_module_extracts_imports_from_submodule(tmp_path):
+    ## test: a module that imports from a submodule
+    app_dir = tmp_path / "app"
+    app_dir.mkdir()
+
+    submodule_dir = app_dir / "submodule"
+    submodule_dir.mkdir()
+
+    submodule_file_path = submodule_dir / "submodule.py"
+    submodule_file_path.write_text(
+        """
+from app.models import User, Post
+from app.config import settings
+import logging
+import pydantic
+import app.other_module.main as other_module_main
+from app.submodule.submodule_function import submodule_function
+
+def submodule_function():
+    print("Hello, World!")
+    logging.info("Hello, World!")
+    pydantic.validate_model(User)
+
+"""
+    )
+
+    module = PythonModule(
+        path = Path("app/submodule/submodule.py"),
+        module_path = "app.submodule.submodule",
+    )
+
+    parsed = parse_module(tmp_path, module)
+
+    assert parsed.path == Path("app/submodule/submodule.py")
+    assert parsed.module_path == "app.submodule.submodule"
+
+    imports = {imp.module_name for imp in parsed.imports}
+
+    assert imports == {
+        "app.models",
+        "app.config",
+        "logging",
+        "pydantic",
+        "app.submodule.submodule_function",
+        "app.other_module.main",
+    }
+
+    assert len(parsed.imports) == 6
+
+
+def test_parse_module_import_not_at_top_level(tmp_path):
+    ## test: a module that imports not at the top-level
+    app_dir = tmp_path / "app"
+    app_dir.mkdir()
+
+    submodule_dir = app_dir / "submodule"
+    submodule_dir.mkdir()
+    
+    submodule_file_path = submodule_dir / "submodule.py"
+    submodule_file_path.write_text(
+        """
+from app.models import User, Post
+
+def submodule_function():
+    import logging
+    logging.info("Hello, World!")
+
+def other_submodule_function():
+    from app.config import settings
+    settings.load_config()
+    return settings
+
+def main():
+    submodule_function()
+    other_submodule_function()
+
+    from app.api import api
+    from app.api.routes import routes
+
+    api.run(routes)
+"""
+    )
+
+    module = PythonModule(
+        path = Path("app/submodule/submodule.py"),
+        module_path = "app.submodule.submodule",
+    )
+
+    parsed = parse_module(tmp_path, module)
+
+    assert parsed.path == Path("app/submodule/submodule.py")
+    assert parsed.module_path == "app.submodule.submodule"
+
+    imports = {imp.module_name for imp in parsed.imports}
+
+    assert imports == {
+        "app.models",
+        "app.config",
+        "app.api",
+        "app.api.routes",
+        "logging",
+    }
+
+    assert len(parsed.imports) == 5
