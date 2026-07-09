d7e0a135f746b6206208e3e0828020ac717de9fd
Adil <Adilbhatti8008@gmail.com>
2026-07-09 15:33:20 -0700

    Created some basic test cases for graph builder

--- backend/tests/test_graph_builder.py
+++ backend/tests/test_graph_builder.py
@@ -1,9 +1,9 @@
 from backend.app.graph.graph_builder import build_graph
-from backend.app.models.import_models import ParsedResult, ParsedModule
+from backend.app.models.import_models import ParsedResult, ParsedModule, ParsedImport
 from backend.app.models.graph_models import Graph, GraphNode, GraphEdge
 from pathlib import Path
 
-def test_graph_builder_basic():
+def test_graph_builder_no_imports_single_module():
     parsed_result = ParsedResult(
         repo_root=Path("test_repo"),
         modules=(
@@ -25,11 +25,85 @@ def test_graph_builder_basic():
     assert graph.unresolved_imports == tuple()
     assert graph.errors == tuple()
 
+def test_graph_builder_no_imports_multiple_modules():
+    parsed_result = ParsedResult(
+        repo_root=Path("test_repo"),
+        modules=(
+            ParsedModule(
+                path=Path("test_repo/module1.py"),
+                module_path="module1",
+                imports=tuple(),
+                error=None,
+            ),
+            ParsedModule(
+                path=Path("test_repo/module2.py"),
+                module_path="module2",
+                imports=tuple(),
+                error=None,
+            ),
+            ParsedModule(
+                path=Path("test_repo/module3.py"),
+                module_path="module3",
+                imports=tuple(),
+                error=None,
+            ),
+        )
+    )
+
+    graph = build_graph(parsed_result)
+    assert isinstance(graph, Graph)
+    assert graph.repo_root == Path("test_repo")
+    assert graph.nodes == (
+        GraphNode("module1", Path("test_repo/module1.py"), 0, 0),
+        GraphNode("module2", Path("test_repo/module2.py"), 0, 0),
+        GraphNode("module3", Path("test_repo/module3.py"), 0, 0),
+    )
+    assert graph.edges == tuple()
+    assert graph.unresolved_imports == tuple()
+    assert graph.errors == tuple()
+
+def test_graph_builder_simple_2_modules_1_import():
+    parsed_result = ParsedResult(
+        repo_root=Path("test_repo"),
+        modules = (
+            ParsedModule(
+                path=Path("test_repo/module1.py"),
+                module_path="module1",
+                imports=tuple(),
+                error=None
+            ),
+            ParsedModule(
+                path=Path("test_repo/module2.py"),
+                module_path="module2",
+                imports=(
+                    ParsedImport(
+                        raw_import="from module1 import variable1",
+                        module_name="module1"
+                    ),
+                ),
+                error=None
+            )
+        )
+    )
+
+    graph = build_graph(parsed_result)
+    assert isinstance(graph, Graph)
+    assert graph.repo_root == Path("test_repo")
+    assert graph.nodes == (
+        GraphNode("module1", Path("test_repo/module1.py"), 1, 0),
+        GraphNode("module2", Path("test_repo/module2.py"), 0, 1),
+    )
+    assert graph.edges == (
+        GraphEdge("module2", "module1", "from module1 import variable1"),
+    )
+    assert graph.unresolved_imports == tuple()
+    assert graph.errors == tuple()
+
 """ 
 Notes:
 Testing scenarios (check for accurate fan_in and fan_out in all cases):
-- No imports + multiple modules
-- No imports + single module
+- No imports + multiple modules DONE
+- No imports + single module DONE
 - Simple 2 modules + 1 import
 - Multiple imports but only 1 module (should go to unresolved imports)
 - Mix of local and external imports
